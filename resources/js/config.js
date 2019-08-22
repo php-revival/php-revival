9f7f04d938800a36f28b4c1bd12cce50c78e5d7e
SerhiiCho <serhiicho@protonmail.com>
2019-08-22 13:13:39 +0300

    Removed line from config.js file

--- resources/js/config.js
+++ resources/js/config.js
@@ -1,5 +1,4 @@
 export default {
-    faviconSrc: 'https://raw.githubusercontent.com/SerhiiCho/php_revival/master/src/images/icon-48.png',
     selectors: {
         codeExamples: '.example-contents .phpcode code span span, #usernotes .note .text .phpcode code span.html span',
         phpVersionInfo: '#layout-content .refnamediv .verinfo',
