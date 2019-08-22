d57f8fc08cb49e4c16b4aeb4b73d6bd609caee81
SerhiiCho <serhiicho@protonmail.com>
2019-08-22 12:42:36 +0300

    Removed  FaviconIcon.js module

--- resources/js/modules/Extension.js
+++ resources/js/modules/Extension.js
@@ -1,12 +1,10 @@
 import PhpInfo from './PhpInfo'
 import PhpCode from './PhpCode'
-import FaviconIcon from './FaviconIcon'
 import SearchBar from './SearchBar'
 
 export default class {
     execute() {
         new SearchBar().focusOnSearchBarOnHomePage()
-        new FaviconIcon().replaceWithCustomIcon()
         new PhpCode().format()
         new PhpInfo().applyStylesAndModifyTheInfo()
     }
