43511905cd0d46109fa2ec8f15620211e04a6ed6
SerhiiCho <serhiicho@protonmail.com>
2023-05-23 10:32:02 +0300

    Changed manifest and background.js to match manifest version 3

--- src/background.js
+++ src/background.js
@@ -1,5 +1,7 @@
-var phpRevival = typeof browser === 'undefined' ? chrome : browser
+const phpRevival = typeof browser === 'undefined' ? chrome : browser
 
-phpRevival.browserAction.onClicked.addListener(function (tab) {
-    phpRevival.tabs.update(tab.id, { url: 'https://php.net' })
+phpRevival.action.onClicked.addListener(function (tab) {
+    phpRevival.tabs.update(tab.id, {
+        url: 'https://php.net',
+    })
 })
