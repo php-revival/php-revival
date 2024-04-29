4f3cbbe429b4826327f81416fbe43e55bbc3c732
SerhiiCho <serhiicho@protonmail.com>
2024-04-29 14:52:39 +0300

    Refactor ts file

--- src/ts/templates/recommendedVideosContainerTemplate.ts
+++ src/ts/templates/recommendedVideosContainerTemplate.ts
@@ -1,17 +1,33 @@
 export default () => {
-    const container = document.createElement('div')
-    container.classList.add('revival-recommended-video-container')
-
-    const targetForCards = document.createElement('div')
-    targetForCards.classList.add('revival-recommended-video-container__inner')
-
-    const btn = document.createElement('button')
-    btn.type = 'button'
-    btn.id = 'revival-show-more-recommended'
-    btn.innerText = 'Show more'
+    const container = createContainer()
+    const targetForCards = createTargetForCards()
+    const btn = createButton()
 
     container.appendChild(targetForCards)
     container.appendChild(btn)
 
     return { container, btn, targetForCards }
+}
+
+function createButton(): HTMLButtonElement {
+    const btn = document.createElement('button')
+    btn.type = 'button'
+    btn.id = 'revival-show-more-recommended'
+    btn.innerText = 'Show more'
+
+    return btn
+}
+
+function createTargetForCards(): HTMLDivElement {
+    const targetForCards = document.createElement('div')
+    targetForCards.classList.add('revival-recommended-video-container__inner')
+
+    return targetForCards
+}
+
+function createContainer(): HTMLDivElement {
+    const container = document.createElement('div')
+    container.classList.add('revival-recommended-video-container')
+
+    return container
 }
