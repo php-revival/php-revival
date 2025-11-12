1f4e51b036f3aad4ac4e5cbe9e008cf3570208b6
SerhiiCho <serhiicho@protonmail.com>
2025-11-12 20:35:26 +0200

    chore: wip on theme switch
    #44

--- src/ts/Adders/ThemeSwitchAdder.ts
+++ src/ts/Adders/ThemeSwitchAdder.ts
@@ -4,7 +4,7 @@ import getImageUrl from '@/modules/getImageUrl'
 import { warn } from '@/modules/err'
 import { ThemeSwitch } from '@/types'
 
-const THEME_SWITCH_BTN_ID = 'theme-switch-btn'
+const SWITCH_BTN_ID = 'theme-switch-btn'
 
 /**
  * Adds light/dark theme switch to the end of the navbar
@@ -23,9 +23,36 @@ export default class ThemeSwitchAdder implements Adder {
         }
 
         const btn = document.createElement('button')
-        btn.id = THEME_SWITCH_BTN_ID
+        btn.id = SWITCH_BTN_ID
         btn.style.background = `url(${getImageUrl('icons/theme-switch-sprite.png')})`
+        btn.addEventListener('click', this.addButtonClickListener.bind(this))
 
         this.target.appendChild(btn)
+    }
+
+    private switchTo(theme: ThemeSwitch): void {
+        const switcher = document.getElementById(SWITCH_BTN_ID) as HTMLButtonElement
+
+        if (!switcher) {
+            warn(`Theme switch with ID ${SWITCH_BTN_ID} doesn't exist`)
+            return
+        }
+
+        switcher.setAttribute('class', '')
+        switcher.classList.add(`theme-switch-${theme}`)
+    }
+
+    private addButtonClickListener(e: MouseEvent): void {
+        const btn = e.target as HTMLButtonElement
+        const isLight = btn.classList.contains('theme-switch-light')
+
+        if (isLight || btn.classList.length === 0) {
+            this.switchTo(ThemeSwitch.Dark)
+            return
+        }
+
+        const isDark = btn.classList.contains('theme-switch-dark')
+
+        this.switchTo(isDark ? ThemeSwitch.Auto : ThemeSwitch.Light)
     }
 }
